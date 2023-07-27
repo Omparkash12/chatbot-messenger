272852a9eac2fbf1c130294c0c4fd9bc186dc1fc
omparkash12 <omparkashbase2brand@gmail.com>
2023-07-27 14:17:56 +0530

    error solved

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import MessengerCustomerChat from "react-messenger-customer-chat";
+import logo from "./logo.svg";
+import "./App.css";
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App() {
   return (
     <div className="App">
-      Chatbot messenger app
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
       <MessengerCustomerChat pageId="119322417839565" appId="572063528240517" />
     </div>
   );
