6a10d0045d69e65cd32b743de9cefe86f075cef6
omparkash12 <omparkashbase2brand@gmail.com>
2023-07-27 16:36:27 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Chat bot</h1>
       <MessengerCustomerChat pageId="119322417839565" appId="572063528240517" />
     </div>
-    
   );
 }
-
-export default App;
