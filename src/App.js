bbca0a46de74f98bde667fb620f83b1f68c1101e
omparkash12 <omparkashbase2brand@gmail.com>
2023-07-27 13:41:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Chatbot messenger app
+      <MessengerCustomerChat pageId="119322417839565" appId="572063528240517" />
     </div>
   );
 }
